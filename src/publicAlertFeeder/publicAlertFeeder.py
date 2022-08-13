a3b43b8f254df5575d864dbfa199bd25bf4b83b1
Volker Krause <vkrause@kde.org>
2022-08-13 19:34:55 +0200

    Add Meteoalarm feeds for Belgium and Spain, add met.no alert feed

--- src/publicAlertFeeder/publicAlertFeeder.py
+++ src/publicAlertFeeder/publicAlertFeeder.py
@@ -15,10 +15,13 @@ requests_cache.install_cache('public-alert-feeder-cache')
 feedReaders = [
     CAPFeedReader('ar-smn', 'https://ssl.smn.gob.ar/CAP/AR.php'),
     CAPFeedReader('at-meteoalarm', 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-atom-austria'),
+    CAPFeedReader('be-meteoalarm', 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-atom-belgium'),
     CAPFeedReader('br-inmet', 'https://apiprevmet3.inmet.gov.br/avisos/rss'),
     MoWaSFeedReader('de-mowas', 'https://warnung.bund.de/bbk.mowas/gefahrendurchsagen.json'),
+    CAPFeedReader('es-meteoalarm', 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-atom-spain'),
     CAPFeedReader('fr-meteoalarm', 'https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-atom-france'),
     CAPFeedReader('in-imd', 'https://cap-sources.s3.amazonaws.com/in-imd-en/rss.xml'),
+    CAPFeedReader('no-met', 'https://alert.met.no/alerts'),
     CAPFeedReader('us-nws', 'https://alerts.weather.gov/cap/us.php?x=0'),
     CAPFeedReader('us-ntwc', 'https://www.tsunami.gov/events/xml/PAAQAtom.xml'),
     CAPFeedReader('us-ptwc', 'https://www.tsunami.gov/events/xml/PHEBAtom.xml'),
