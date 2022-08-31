b833050238959b8f13dfbaf1af538efa67143afa
Volker Krause <vkrause@kde.org>
2022-08-31 18:16:04 +0200

    Add CAP feeds for CA, EE, LT, LV, MN, PH and UG

--- src/tools/meteoalarm-geocode-expander.py
+++ src/tools/meteoalarm-geocode-expander.py
@@ -14,7 +14,7 @@ arguments = parser.parse_args()
 
 os.makedirs(os.path.join(arguments.output, 'EMMA_ID'), exist_ok=True)
 
-include_countries = ['AT', 'BE', 'CH', 'CZ', 'DK', 'ES', 'FR', 'IT', 'NL', 'PL', 'PT']
+include_countries = ['AT', 'BE', 'CH', 'CZ', 'DK', 'EE', 'ES', 'FR', 'IT', 'LT', 'LV', 'NL', 'PL', 'PT']
 include_aliases = [ 'FIPS', 'NUTS2', 'NUTS3' ]
 
 geocodesFile = open('geocodes.json')
