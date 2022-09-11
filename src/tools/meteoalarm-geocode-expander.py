7b96c8d7da0d5bd8e110d50ac4068cf12c53dfa1
Volker Krause <vkrause@kde.org>
2022-09-11 10:54:46 +0200

    Add weather alert feeds for IE, LS, MG, ML, MU, MW, NE, RO, SK

--- src/tools/meteoalarm-geocode-expander.py
+++ src/tools/meteoalarm-geocode-expander.py
@@ -14,7 +14,7 @@ arguments = parser.parse_args()
 
 os.makedirs(os.path.join(arguments.output, 'EMMA_ID'), exist_ok=True)
 
-include_countries = ['AT', 'BE', 'BG', 'CH', 'CZ', 'DK', 'EE', 'ES', 'FI', 'FR', 'GR', 'HU', 'IT', 'LT', 'LU', 'LV', 'NL', 'PL', 'PT', 'SE']
+include_countries = ['AT', 'BE', 'BG', 'CH', 'CZ', 'DK', 'EE', 'ES', 'FI', 'FR', 'GR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'NL', 'PL', 'PT', 'RO', 'SE', 'SK']
 include_aliases = [ 'FIPS', 'NUTS2', 'NUTS3' ]
 
 geocodesFile = open('geocodes.json')
